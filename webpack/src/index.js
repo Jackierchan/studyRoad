39d7b6fbb9a0c029640be131a74e98181517ce2b
jackierchan <1192424515@qq.com>
2018-08-08 15:21:56 +0800

    8.8.1

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,12 @@
 import css from './css/style.css';
+import sass from './css/common.scss';
+import $ from jQuery;
 var odiv=document.getElementById('div1');
-odiv.innerHTML = 'hello world';
+// odiv.innerHTML = 'hello world';
 // console.log(odiv.innerHTML);
+let l=20;
+console.log(l);
+var s=new Set();
+s.add(111);
+s.add(true);
+console.log(s);
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,11 +2,13 @@ const path = require("path");
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
-module.exports={
+const ExtractTextPlugin=require("extract-text-webpack-plugin");
+const glob=require('glob');
+const PurifyCSSPlugin=require("purifycss-webpack");
+const entry = require("./entry_webpack.js");
+    module.exports={
+    entry:entry,
     mode: 'development',
-    entry:{
-        'index':'./src/index.js'
-    },
     output:{
         path:path.resolve(__dirname,'dist'),
         filename:'[name].js',
@@ -17,11 +19,14 @@ module.exports={
             {
                 test:/\.css$/,
                 // use:['style-loader','css-loader']
-                use:ExtractTextWebpackPlugin.extract({
-                    fallback:"style-loader",
-                    use:"css-loader"
-                })
-            },{
+                use: ExtractTextPlugin.extract({
+                        fallback:"style-loader",
+                        use:[
+                            {
+                                loader:"css-loader",
+                                options:{importLoaders:1}
+                            },'postcss-loader']
+            })},{
                 test:/\.(png|jpg|gif)/,
                 use:[
                     {
@@ -34,21 +39,63 @@ module.exports={
                 ]
             }
             ,{
-            test:/\.(htm|html)$/i,
-            loader:'html-withimg-loader'
-            }
-        ]
+                test:/\.(htm|html)$/i,
+                loader:'html-withimg-loader'
+            },{
+                test:/\.scss/,
+                // use: [
+                //     {
+                //         loader: 'style-loader'
+                //     },
+                //     {
+                //          loader:'css-loader'
+                //     },
+                //     {
+                //          loader:'sass-loader'
+                //     }
+                // ]
+                use:ExtractTextPlugin.extract({
+                use:[
+                    {
+                        loader:'css-loader'
+                    },{
+                    loader:'sass-loader'
+                    }
+                ],
+                fallback:'style-loader'
+
+            })
+
+
     },
+            {
+                test:/\.(jsx|js)$/,
+                use:{
+                    loader:'babel-loader',
+                    options:{
+                        presets:[
+                            "es2015"
+                        ]
+                    }
+                },
+                exclude:/node_modules/
+            }
+            ]},
     plugins:[
         new webpack.HotModuleReplacementPlugin(),
         new HtmlWebpackPlugin({
+            chunks:['index'],
           minify:{
               removeAttributeQuotes:true
           },
             hash:true,
             template:'./src/index.html'
         }),
-        new ExtractTextWebpackPlugin("./css/style.css")
+        new ExtractTextWebpackPlugin("./css/style.css"),
+        new PurifyCSSPlugin({
+            paths:glob.sync(path.join(__dirname,'src/*html'))
+        }),
+        new webpack.BannerPlugin("大大大大大大")
     ],
    devServer:{
        contentBase:path.resolve(__dirname,'dist'),
@@ -58,4 +105,4 @@ module.exports={
        hot:true,
        open:true
    }
-};
+}
