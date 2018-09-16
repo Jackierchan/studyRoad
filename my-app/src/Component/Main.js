069ad6532ad8033ccc455ccd1c8fce6668f13d2d
jackierchan <1192424515@qq.com>
2018-09-16 16:03:38 +0800

    9.16.1

---
+++ my-app/src/Component/Main.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+import {BrowserRouter as Router,Route,Link} from 'react-router-dom';
+
+
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                1111111111
+            </div>
+        );
+    }
+}
+export default Main
