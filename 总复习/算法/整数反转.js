70bb8aca5b98bad620565b6d12152585f24bfbe6
Jackierchan <chenlei16@xiaomi.com>
2022-03-23 17:46:49 +0800

    算法

---
+++ 总复习/算法/整数反转.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let last
+    let result =0
+    let flag =1
+    if(x<0){
+        flag = 0
+        x=Math.abs(x)
+    }
+    while(x!=0){
+        last =x%10
+        result = result*10+last
+        x=Math.floor(x/10)
+    }
+     if(flag==0){
+        result =-result
+     } 
+    if(-Math.pow(2,31) <= result&&result <=( Math.pow(2,31) - 1)){
+        return result
+    }else{
+        return 0
+    }
+
+};
